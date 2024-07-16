221cfcb5d88cb87b20cbe19606d7e4d79cd023e1
Lucas Ferrero <lucasdavidferrero@gmail.com>
2024-07-16 12:17:57 -0300

    agrego transacción.-

--- functions/src/QuasarApp/services/ProcesosApiAikon/ProcesoDiarioParaSincronizarArticulos.ts
+++ functions/src/QuasarApp/services/ProcesosApiAikon/ProcesoDiarioParaSincronizarArticulos.ts
@@ -89,7 +89,7 @@ export class ProcesoDiarioCompletoParaSincronizarArticulos extends ProcesoBase {
                     aik_ar_alto: articulo.ar_alto,
                     aik_ar_ancho: articulo.ar_ancho,
                     aik_ar_profundo: articulo.ar_profundo,
-                    aik_ar_color: articulo.ar_codigo,
+                    aik_ar_color: articulo.ar_color,
                     aik_ar_peso: articulo.ar_peso,
                     aik_ar_descria: articulo.ar_descria,
                     aik_ar_mesesgarantia: articulo.AR_MESESGARANTIA,
@@ -125,98 +125,100 @@ export class ProcesoDiarioCompletoParaSincronizarArticulos extends ProcesoBase {
             const prismaCreate: Promise<any>[] = []
             const prismaUpdatesNoHabilitados: Promise<any>[] = []
             const prismaUpdatesHabilitados: Promise<any>[] = []
-            listadoArticulosConPreciosConvertidoFromAPI.forEach(articuloAikonApi => {
-                // Los artículos que están en la API pero no en MySQL -> CREATE.
-                const indexArticuloPrecio = aikonArticulosMcHogar.findIndex(articuloPrecio => {
-                    return articuloPrecio.aik_ar_codigo === articuloAikonApi.aik_ar_codigo
-                })
-                if (indexArticuloPrecio !== -1) {
-                    const prismaCreateArticulo = prisma.aikon_articulo.create({
-                        data: {
-                            aik_ar_codigo: articuloAikonApi.aik_ar_codigo,
-                            aik_ar_publicarweb: 'S',
-                            aik_ar_descri: articuloAikonApi.aik_ar_descri,
-                            aik_ar_memo: articuloAikonApi.aik_ar_memo,
-                            aik_ar_alto: articuloAikonApi.aik_ar_alto,
-                            aik_ar_ancho: articuloAikonApi.aik_ar_ancho,
-                            aik_ar_profundo: articuloAikonApi.aik_ar_profundo,
-                            aik_ar_color: articuloAikonApi.aik_ar_color,
-                            aik_ar_peso: articuloAikonApi.aik_ar_peso,
-                            aik_ar_descria: articuloAikonApi.aik_ar_descria,
-                            aik_ar_mesesgarantia: articuloAikonApi.aik_ar_mesesgarantia,
-                            aik_ar_cosnet: articuloAikonApi.aik_ar_cosnet,
-                            aik_ap_utilidad: articuloAikonApi.aik_ap_utilidad,
-                            aik_ap_impuesto_interno: articuloAikonApi.aik_ap_impuesto_interno,
-                            aik_iva_porcen: articuloAikonApi.aik_iva_porcen,
-                            aik_stock_total: articuloAikonApi.aik_stock_total,
-                            aik_ap_precio_iva: articuloAikonApi.aik_ap_precio_iva,
-                            aik_ar_fechamodif: articuloAikonApi.aik_ar_fechamodif,
-                            aik_ar_fecha_alta: articuloAikonApi.aik_ar_fecha_alta,
-                            aik_fa_codigo: articuloAikonApi.aik_fa_codigo,
-                            aik_ma_codigo: articuloAikonApi.aik_ma_codigo,
-                            aik_re1_codigo: articuloAikonApi.aik_re1_codigo,
-                            aik_re2_codigo: articuloAikonApi.aik_re2_codigo,
-                            aik_esa_codigo: articuloAikonApi.aik_esa_codigo,
-                        }
-                    })
-                    prismaCreate.push(prismaCreateArticulo)
-                }
-            })
 
-            aikonArticulosMcHogar.forEach(articuloMcHogar => {
-                const articuloApiAikon = listadoArticulosConPreciosConvertidoFromAPIIndex[articuloMcHogar.aik_ar_codigo]
-                const articuloNoHabilitadoApiAikon = dtTablaArticulosNoHabilitadosAikonApiIndex[articuloMcHogar.aik_ar_codigo]
-                if(!articuloApiAikon) {
-                    // Artículo se encuentra en DB pero no en la API Aikon.
-                    const updateArticuloNoHabilitado = prisma.aikon_articulo.update({
-                        where: { aik_ar_codigo: articuloMcHogar.aik_ar_codigo },
-                        data: { aik_esa_codigo: articuloNoHabilitadoApiAikon.ESA_CODIGO, aik_ar_publicarweb: articuloNoHabilitadoApiAikon.AR_PUBLICARWEB }
+            await prisma.$transaction( async(tx) => {
+                listadoArticulosConPreciosConvertidoFromAPI.forEach(articuloAikonApi => {
+                    // Los artículos que están en la API pero no en MySQL -> CREATE.
+                    const indexArticuloPrecio = aikonArticulosMcHogar.findIndex(articuloPrecio => {
+                        return articuloPrecio.aik_ar_codigo === articuloAikonApi.aik_ar_codigo // [hacerlo con índices. es más rápido]
                     })
-                    prismaUpdatesNoHabilitados.push(updateArticuloNoHabilitado)
-                } else {
-                    // Artículo se encuentra en API y en la DB Mysql.
-                    if (articuloApiAikon.aik_ar_fechamodif !== articuloMcHogar.aik_ar_fechamodif) {
-                        // Sincronizar artículos.
-                        const updateArticuloHabilitado = prisma.aikon_articulo.update({
-                            where: { aik_ar_codigo: articuloMcHogar.aik_ar_codigo },
+                    if (indexArticuloPrecio === -1) {
+                        const prismaCreateArticulo = tx.aikon_articulo.create({
                             data: {
-                                aik_ar_descri: articuloApiAikon.aik_ar_descri,
-                                aik_ar_memo: articuloApiAikon.aik_ar_memo,
-                                aik_ar_alto: articuloApiAikon.aik_ar_alto,
-                                aik_ar_ancho: articuloApiAikon.aik_ar_ancho,
-                                aik_ar_profundo: articuloApiAikon.aik_ar_profundo,
-                                aik_ar_color: articuloApiAikon.aik_ar_codigo,
-                                aik_ar_peso: articuloApiAikon.aik_ar_peso,
-                                aik_ar_descria: articuloApiAikon.aik_ar_descria,
-                                aik_ar_mesesgarantia: articuloApiAikon.aik_ar_mesesgarantia,
-                                aik_ar_cosnet: articuloApiAikon.aik_ar_cosnet,
-                                aik_ap_utilidad: articuloApiAikon.aik_ap_utilidad,
-                                aik_ap_impuesto_interno: articuloApiAikon.aik_ap_impuesto_interno,
-                                aik_iva_porcen: articuloApiAikon.aik_iva_porcen,
-                                aik_stock_total: articuloApiAikon.aik_stock_total,
-                                aik_ap_precio_iva: articuloApiAikon.aik_ap_precio_iva,
-                                aik_ar_fechamodif: articuloApiAikon.aik_ar_fechamodif,
-                                aik_ar_fecha_alta: articuloApiAikon.aik_ar_fecha_alta,
-                                aik_fa_codigo: articuloApiAikon.aik_fa_codigo,
-                                aik_ma_codigo: articuloApiAikon.aik_ma_codigo,
-                                aik_re1_codigo: articuloApiAikon.aik_re1_codigo,
-                                aik_re2_codigo: articuloApiAikon.aik_re2_codigo,
-                                aik_esa_codigo: articuloApiAikon.aik_esa_codigo
-                            }                           
+                                aik_ar_codigo: articuloAikonApi.aik_ar_codigo,
+                                aik_ar_publicarweb: 'S',
+                                aik_ar_descri: articuloAikonApi.aik_ar_descri,
+                                aik_ar_memo: articuloAikonApi.aik_ar_memo,
+                                aik_ar_alto: articuloAikonApi.aik_ar_alto,
+                                aik_ar_ancho: articuloAikonApi.aik_ar_ancho,
+                                aik_ar_profundo: articuloAikonApi.aik_ar_profundo,
+                                aik_ar_color: articuloAikonApi.aik_ar_color,
+                                aik_ar_peso: articuloAikonApi.aik_ar_peso,
+                                aik_ar_descria: articuloAikonApi.aik_ar_descria,
+                                aik_ar_mesesgarantia: articuloAikonApi.aik_ar_mesesgarantia,
+                                aik_ar_cosnet: articuloAikonApi.aik_ar_cosnet,
+                                aik_ap_utilidad: articuloAikonApi.aik_ap_utilidad,
+                                aik_ap_impuesto_interno: articuloAikonApi.aik_ap_impuesto_interno,
+                                aik_iva_porcen: articuloAikonApi.aik_iva_porcen,
+                                aik_stock_total: articuloAikonApi.aik_stock_total,
+                                aik_ap_precio_iva: articuloAikonApi.aik_ap_precio_iva,
+                                aik_ar_fechamodif: articuloAikonApi.aik_ar_fechamodif,
+                                aik_ar_fecha_alta: articuloAikonApi.aik_ar_fecha_alta,
+                                aik_fa_codigo: articuloAikonApi.aik_fa_codigo,
+                                aik_ma_codigo: articuloAikonApi.aik_ma_codigo,
+                                aik_re1_codigo: articuloAikonApi.aik_re1_codigo,
+                                aik_re2_codigo: articuloAikonApi.aik_re2_codigo,
+                                aik_esa_codigo: articuloAikonApi.aik_esa_codigo,
+                            }
                         })
-                        prismaUpdatesHabilitados.push(updateArticuloHabilitado)
+                        prismaCreate.push(prismaCreateArticulo)
                     }
-                }
+                })
+    
+                aikonArticulosMcHogar.forEach(articuloMcHogar => {
+                    const articuloApiAikon = listadoArticulosConPreciosConvertidoFromAPIIndex[articuloMcHogar.aik_ar_codigo]
+                    const articuloNoHabilitadoApiAikon = dtTablaArticulosNoHabilitadosAikonApiIndex[articuloMcHogar.aik_ar_codigo]
+                    if(!articuloApiAikon) {
+                        // Artículo se encuentra en DB pero no en la API Aikon.
+                        const updateArticuloNoHabilitado = tx.aikon_articulo.update({
+                            where: { aik_ar_codigo: articuloMcHogar.aik_ar_codigo },
+                            data: { aik_esa_codigo: articuloNoHabilitadoApiAikon.ESA_CODIGO, aik_ar_publicarweb: articuloNoHabilitadoApiAikon.AR_PUBLICARWEB }
+                        })
+                        prismaUpdatesNoHabilitados.push(updateArticuloNoHabilitado)
+                    } else {
+                        // Artículo se encuentra en API y en la DB Mysql.
+                        if (articuloApiAikon.aik_ar_fechamodif !== null && BigInt(articuloApiAikon.aik_ar_fechamodif) !== articuloMcHogar.aik_ar_fechamodif) {
+                            // Sincronizar artículos únicamente si las fechas modificadas son distintas. Se convierte a BigInt la fecha de la API para que en la comparación sea compatible los tipos de datos.
+                            const updateArticuloHabilitado = tx.aikon_articulo.update({
+                                where: { aik_ar_codigo: articuloMcHogar.aik_ar_codigo },
+                                data: {
+                                    aik_ar_publicarweb: 'S',
+                                    aik_ar_descri: articuloApiAikon.aik_ar_descri,
+                                    aik_ar_memo: articuloApiAikon.aik_ar_memo,
+                                    aik_ar_alto: articuloApiAikon.aik_ar_alto,
+                                    aik_ar_ancho: articuloApiAikon.aik_ar_ancho,
+                                    aik_ar_profundo: articuloApiAikon.aik_ar_profundo,
+                                    aik_ar_color: articuloApiAikon.aik_ar_color,
+                                    aik_ar_peso: articuloApiAikon.aik_ar_peso,
+                                    aik_ar_descria: articuloApiAikon.aik_ar_descria,
+                                    aik_ar_mesesgarantia: articuloApiAikon.aik_ar_mesesgarantia,
+                                    aik_ar_cosnet: articuloApiAikon.aik_ar_cosnet,
+                                    aik_ap_utilidad: articuloApiAikon.aik_ap_utilidad,
+                                    aik_ap_impuesto_interno: articuloApiAikon.aik_ap_impuesto_interno,
+                                    aik_iva_porcen: articuloApiAikon.aik_iva_porcen,
+                                    aik_stock_total: articuloApiAikon.aik_stock_total,
+                                    aik_ap_precio_iva: articuloApiAikon.aik_ap_precio_iva,
+                                    aik_ar_fechamodif: articuloApiAikon.aik_ar_fechamodif,
+                                    aik_ar_fecha_alta: articuloApiAikon.aik_ar_fecha_alta,
+                                    aik_fa_codigo: articuloApiAikon.aik_fa_codigo,
+                                    aik_ma_codigo: articuloApiAikon.aik_ma_codigo,
+                                    aik_re1_codigo: articuloApiAikon.aik_re1_codigo,
+                                    aik_re2_codigo: articuloApiAikon.aik_re2_codigo,
+                                    aik_esa_codigo: articuloApiAikon.aik_esa_codigo
+                                }                           
+                            })
+                            prismaUpdatesHabilitados.push(updateArticuloHabilitado)
+                        }
+                    }
+    
+                })
 
+                const resultCreations = await Promise.all(prismaCreate)
+                const resultUpdateHabilitados = await Promise.all(prismaUpdatesHabilitados)
+                const resultUpdateNoHabilitados = await Promise.all(prismaUpdatesNoHabilitados)
+                console.log(resultCreations, resultUpdateHabilitados, resultUpdateNoHabilitados)
+                console.log('Proceso de sync finalizado correctamente...')
             })
-
-            // Ejecutar UPDATE y CREATE.
-            const resultCreations = await Promise.all(prismaCreate)
-            const resultUpdateHabilitados = await Promise.all(prismaUpdatesHabilitados)
-            const resultUpdateNoHabilitados = await Promise.all(prismaUpdatesNoHabilitados)
-
-            console.log(resultCreations, resultUpdateHabilitados, resultUpdateNoHabilitados)
-            console.log('Proceso de sync finalizado correctamente...')
         }
     }
 }
