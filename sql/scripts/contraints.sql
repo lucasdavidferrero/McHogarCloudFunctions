2aab4064e1de413d4fa905fbc80f12041665b268
Lucas Ferrero <lucasdavidferrero@gmail.com>
2024-07-26 11:00:29 -0300

    refactor constraints....

--- sql/scripts/contraints.sql
+++ sql/scripts/contraints.sql
@@ -27,21 +27,35 @@ END //
 DELIMITER ;
 
 /* Eliminamos check constraint (si existen) */
-CALL DropCheckConstraintIfExists('articulo_precio', 'arp_utilidad_web_chk');
-CALL DropCheckConstraintIfExists('articulo_precio', 'arp_utilidad_ofer_chk');
-CALL DropCheckConstraintIfExists('articulo_precio', 'arp_utilidad_ofer_fecha_hasta_chk');
-CALL DropCheckConstraintIfExists('articulo_precio', 'arp_utilidad_ofer_stock_hasta_chk');
-CALL DropCheckConstraintIfExists('articulo_precio', 'arp_descuento_chk');
+CALL DropCheckConstraintIfExists('articulo_precio', 'articulo_precio_CHECK_ARP_UTILIDAD_WEB');
+CALL DropCheckConstraintIfExists('articulo_precio', 'articulo_precio_CHECK_ARP_UTILIDAD_OFER');
+CALL DropCheckConstraintIfExists('articulo_precio', 'articulo_precio_CHECK_ARP_UTILIDAD_OFER_FECHA_HASTA');
+CALL DropCheckConstraintIfExists('articulo_precio', 'articulo_precio_CHECK_ARP_UTILIDAD_OFER_STOCK_HASTA');
+CALL DropCheckConstraintIfExists('articulo_precio', 'articulo_precio_CHECK_ARP_DESCUENTO');
+CALL DropCheckConstraintIfExists('proceso_info', 'proceso_info_CHECK_ESTADO_EJECUCION');
+CALL DropCheckConstraintIfExists('proceso_info_detalle', 'proceso_info_detalle_CHECK_ESTADO_EJECUCION');
 
 /* Creamos los CHECK CONSTRAINT. Estas reflejan reglas de negocio. */
+
+/* CHECK CONSTRAINT para articulos */
 ALTER TABLE articulo_precio
-ADD CONSTRAINT arp_utilidad_web_chk 
+ADD CONSTRAINT `articulo_precio_CHECK_ARP_UTILIDAD_WEB`
 	CHECK (arp_utilidad_web >= 0),
-ADD CONSTRAINT arp_utilidad_ofer_chk 
+ADD CONSTRAINT `articulo_precio_CHECK_ARP_UTILIDAD_OFER`
 	CHECK ( ((arp_utilidad_ofer >= 0) AND (arp_utilidad_ofer_fecha_hasta IS NOT NULL)) OR arp_utilidad_ofer IS NULL),
-ADD CONSTRAINT arp_utilidad_ofer_fecha_hasta_chk
+ADD CONSTRAINT `articulo_precio_CHECK_ARP_UTILIDAD_OFER_FECHA_HASTA`
 	CHECK((arp_utilidad_ofer_fecha_hasta IS NOT NULL AND arp_utilidad_ofer IS NOT NULL) OR (arp_utilidad_ofer_fecha_hasta IS NULL AND arp_utilidad_ofer IS NULL)),
-ADD CONSTRAINT arp_utilidad_ofer_stock_hasta_chk
+ADD CONSTRAINT `articulo_precio_CHECK_ARP_UTILIDAD_OFER_STOCK_HASTA`
 	CHECK((arp_utilidad_ofer_stock_hasta IS NOT NULL AND arp_utilidad_ofer IS NOT NULL) OR arp_utilidad_ofer_stock_hasta IS NULL),
-ADD CONSTRAINT arp_descuento_chk 
+ADD CONSTRAINT `articulo_precio_CHECK_ARP_DESCUENTO`
 	CHECK(arp_descuento > 0);
+
+/* CHECK CONSTRAINT para tablas de procesos. */
+ALTER TABLE proceso_info
+ADD CONSTRAINT `proceso_info_CHECK_ESTADO_EJECUCION`
+	CHECK (estado_ejecucion IN ('Procesando', 'Finalizado'));
+
+ALTER TABLE proceso_info_detalle
+ADD CONSTRAINT `proceso_info_detalle_CHECK_ESTADO_EJECUCION`
+	CHECK (estado_ejecucion IN ('Procesando', 'Finalizado'));
+
