e5f875c9ba346cd4a61b4df44effb260d8762064
Lucas Ferrero <lucasdavidferrero@gmail.com>
2024-07-31 09:02:19 -0300

    se aplica un nuevo filtro para unicamente actualizar si algún campo relevante fué modificado en Aikon.

--- functions/src/server/servicios/SyncArticuloInfoRelevante.ts
+++ functions/src/server/servicios/SyncArticuloInfoRelevante.ts
@@ -6,22 +6,30 @@ import { DateUtils } from '../../utils/DateUtils';
 
 const LP_CODIGO_PRECIO_VENTA_PUBLICO = '01';
 
-/*
-    [X] Obtener Todos los artículos Prisma
-    [X] Convertir Estructuras.
-    [X] Hacer update con prisma únicamente de los artículos que están guardados en mysql.  
-*/
 interface fetchAllAikonArticulosWithSelectSubsetReturnValue {
     aik_ar_codigo: string
     aik_ar_fechamodif: BigInt
+    aik_stock_total: number
+    aik_ar_cosnet: Prisma.Decimal
+    aik_iva_porcen: Prisma.Decimal
+    aik_ap_precio_iva: Prisma.Decimal
+    aik_ap_impuesto_interno: Prisma.Decimal
+    aik_ap_utilidad: Prisma.Decimal
 }
+
+/**
+ * Clase responsable de preparar las actualizaciones de los Artículos ya existentes en la base de datos MySQL.
+ * Únicamente se actualizan los artículos que tengan al menos un valor distinto comparado con su equivalente en la API de aikon, en los 
+ * campos: aik_stock_total, aik_ar_cosnet, aik_iva_porcen, aik_ap_precio_iva, aik_ap_impuesto_interno, aik_ap_utilidad.
+ */
 export class SyncArticuloInfoRelevante {
     static async prepararSincronizacion (token: string): Promise<Prisma.PrismaPromise<aikon_articulo>[]> {
 
         const [responseDtTablaArticulo, responseDtTablaPrecios, aikonArticulosMcHogarArray ] = await Promise.all([
             AikonApiDtTablaService.fetchArticulos(token),
             AikonApiDtTablaService.fetchPrecios(token),
-            PrismaService.fetchAllAikonArticulosWithSelectSubset({ aik_ar_codigo: true, aik_ar_fechamodif: true })
+            PrismaService.fetchAllAikonArticulosWithSelectSubset({ aik_ar_codigo: true, aik_ar_fechamodif: true, aik_stock_total: true, 
+                aik_ar_cosnet: true, aik_iva_porcen: true, aik_ap_precio_iva: true, aik_ap_impuesto_interno: true, aik_ap_utilidad: true })
         ])
 
         const dtTablaArticuloDataAikonApi = responseDtTablaArticulo.data
@@ -35,19 +43,26 @@ export class SyncArticuloInfoRelevante {
         const articulosConPrecioConvertidosToPrisma = this.transformarConvertirArticuloPrismaToArticuloPrecioPrisma(articulosPrisma, dtTablaPreciosDataAikonApi)
 
         /* Se filtran los artículos que ya existen en la base de datos de Prisma y que la fecha de modificación sea distinta. */
-        const articuloFiltradoParaActualizar = articulosConPrecioConvertidosToPrisma.filter((articulo) => {
+        const articulosExistentesEnPrismaConFechaModifDistintasFiltrados = articulosConPrecioConvertidosToPrisma.filter((articulo) => {
             return aikonArticulosMcHogarObjectWithIds[articulo.id] && Number(aikonArticulosMcHogarObjectWithIds[articulo.id].aik_ar_fechamodif) !== articulo.data.aik_ar_fechamodif
         })
-        /*  TODO:
-            Aplicar un filtro más (tomando articuloFiltradoParaActualizar) que unicamente deje los artículos
-            que tengan distintos valores en uno de los atributos que se desea sincronizar: 
-            aik_stock_total, aik_ar_cosnet, aik_iva_porcen, aik_ar_fechamodif, aik_ap_precio_iva, aik_ap_impuesto_interno, aik_ap_utilidad
-            Esto es más eficiente. ya que la fecha de modificación toma un nuevo valor cuando se cambia cualquiera de los campos
-            de un artículo.
+
+        /* 
+            Si al menos uno de los campos de la API comparada con la de Prisma es !==, se actualiza el artículo.
         */
+        const articulosFiltradosUnicamenteValoresCamposDistintos = articulosExistentesEnPrismaConFechaModifDistintasFiltrados.filter((articulo) => {
+            return (aikonArticulosMcHogarObjectWithIds[articulo.id].aik_stock_total !== articulo.data.aik_stock_total                               ||
+                    aikonArticulosMcHogarObjectWithIds[articulo.id].aik_ar_cosnet.toNumber() !== articulo.data.aik_ar_cosnet                        ||
+                    aikonArticulosMcHogarObjectWithIds[articulo.id].aik_iva_porcen.toNumber() !== articulo.data.aik_iva_porcen                      ||
+                    aikonArticulosMcHogarObjectWithIds[articulo.id].aik_ap_precio_iva.toNumber() !== articulo.data.aik_ap_precio_iva                ||
+                    aikonArticulosMcHogarObjectWithIds[articulo.id].aik_ap_impuesto_interno.toNumber() !== articulo.data.aik_ap_impuesto_interno    ||
+                    aikonArticulosMcHogarObjectWithIds[articulo.id].aik_ap_utilidad.toNumber() !== articulo.data.aik_ap_utilidad
+
+            )
+        })
 
         const articulosPreparadosParaActualizar: PrismaPromise<aikon_articulo>[] = []
-        articuloFiltradoParaActualizar.forEach((articulo) => {
+        articulosFiltradosUnicamenteValoresCamposDistintos.forEach((articulo) => {
             articulosPreparadosParaActualizar.push(PrismaService.updateAikonArticulo(articulo.id, articulo.data))
         })
 
