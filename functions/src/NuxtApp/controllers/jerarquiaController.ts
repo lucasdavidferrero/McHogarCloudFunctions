d2467f5f979c94ec41eb741243ade12ab17a2104
Lucas Ferrero <lucasdavidferrero@gmail.com>
2024-08-08 12:24:42 -0300

    controller

--- functions/src/NuxtApp/controllers/jerarquiaController.ts
+++ functions/src/NuxtApp/controllers/jerarquiaController.ts
@@ -1,8 +1,12 @@
 import { Request, Response } from "express"
+import { JerarquiaPrismaService } from "../../SharedApp/services/Prisma/JerarquiaPrismaService"
 
 const obtenerJerarquiasCompleto = async (req: Request, res: Response) => {
+    const result = await JerarquiaPrismaService.obtenerJerarquiaCompleta()
     res.send({
-        OK: true
+        estado: 'satisfactorio',
+        mensaje: 'Jerarquía completa excluyendo la categoría "SIN CATEGORÍA"',
+        data: result
     })
 }
 
