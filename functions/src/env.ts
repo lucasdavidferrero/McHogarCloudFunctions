0d4d5fd298e64cdbabeebeb0c170233ab925c861
Lucas Ferrero <lucasdavidferrero@gmail.com>
2024-08-21 10:54:59 -0300

    logica para configurar los envs.

---
+++ functions/src/env.ts
@@ -0,0 +1,19 @@
+import dotenv from 'dotenv';
+import functions from 'firebase-functions';
+
+// Load the correct .env file based on the environment
+const environment = functions.config().environment || 'development';
+dotenv.config({ path: `../.env.${environment}` });
+
+export const FIRE_API_KEY = process.env.FIRE_API_KEY || '';
+export const FIRE_AUTH_DOMAIN = process.env.FIRE_AUTH_DOMAIN || '';
+export const FIRE_PROJECT_ID = process.env.FIRE_PROJECT_ID || '';
+export const FIRE_STORAGE_BUCKET = process.env.FIRE_STORAGE_BUCKET || '';
+export const FIRE_MESSAGING_SENDER_ID = process.env.FIRE_MESSAGING_SENDER_ID || '';
+export const FIRE_APP_ID = process.env.FIRE_APP_ID || '';
+export const FIRE_MEASUREMENT_ID = process.env.FIRE_MEASUREMENT_ID || '';
+export const GOOGLE_APPLICATION_CREDENTIALS_JSON_FILENAME = process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON_FILENAME || '';
+
+export const DATABASE_URL = process.env.DATABASE_URL || '';
+
+export default process.env;
--- functions/src/firebase.ts
+++ functions/src/firebase.ts
@@ -1,12 +1,22 @@
 import { getFirestore } from "firebase-admin/firestore";
 import { getStorage } from "firebase-admin/storage";
 import { initializeApp, cert  } from "firebase-admin/app";
-const serviceAccount = require('../../cloudfunctions-express-templat-firebase-adminsdk-zkh4i-3b7f67997f.json')
+import { FIRE_API_KEY, FIRE_AUTH_DOMAIN, FIRE_PROJECT_ID, FIRE_STORAGE_BUCKET, FIRE_MESSAGING_SENDER_ID, FIRE_APP_ID, FIRE_MEASUREMENT_ID, GOOGLE_APPLICATION_CREDENTIALS_JSON_FILENAME } from './env'
+const serviceAccount = require(`../../${GOOGLE_APPLICATION_CREDENTIALS_JSON_FILENAME}`)
+// const serviceAccount = require('../../cloudfunctions-express-templat-firebase-adminsdk-zkh4i-3b7f67997f.json')
 
-const firebaseApp = initializeApp({
-    credential: cert(serviceAccount),
-    storageBucket: 'cloudfunctions-express-templat.appspot.com'
-});
+const firebaseConfig = {
+    apiKey: FIRE_API_KEY,
+    authDomain: FIRE_AUTH_DOMAIN,
+    projectId: FIRE_PROJECT_ID,
+    storageBucket: FIRE_STORAGE_BUCKET,
+    messagingSenderId: FIRE_MESSAGING_SENDER_ID,
+    appId: FIRE_APP_ID,
+    measurementId: FIRE_MEASUREMENT_ID,
+    credential: cert(serviceAccount)
+  };
+
+const firebaseApp = initializeApp(firebaseConfig);
 
 const firestore = getFirestore(firebaseApp);
 const defaultStorage = getStorage().bucket()
